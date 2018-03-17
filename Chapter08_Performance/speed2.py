102ef448ac1f2050eabccab7cc69f4c92033be80
nschorgh <norbert@psi.edu>
2018-03-16 16:48:33 -1000

    updates

--- Chapter08_Performance/speed2.py
+++ Chapter08_Performance/speed2.py
@@ -2,10 +2,11 @@ import math
 import numpy
 
 a = 1.
-for i in np.arange(1,1000000000/100):
-    a=a+12.1
+for i in numpy.arange(1,1000000000/100):
+    #a=a+12.1
     #a=a*1.0000001
     #a=a/1.0000001
-    #a=math.sin(3.*a)
-
+    a=math.sin(3.*a)
+    #a=numpy.sin(3.*a)
+    
 print a
