7d56ff47fbea9b4b25918f08ae53aa1e6cda861c
nschorgh <norbert@psi.edu>
2018-03-30 00:49:25 -1000

    updates

--- Chapter08_Performance/speed3.py
+++ Chapter08_Performance/speed3.py
@@ -1,12 +1,11 @@
-# Python2
+# Python3 version of speed.py
 import math 
 
 a = 1.
-for i in range(1,1000000000/10):
-#for i in xrange(1,1000000000/10):
+for i in range(1,100000000):
     a=a+12.1
     #a=a*1.0000001
     #a=a/1.0000001
     #a=math.sin(3.*a)
 
-print a
+print(a)
